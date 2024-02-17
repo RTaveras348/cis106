4c65227a0b3092cf56db4bbaa7b7396fc5fe8702
RTaveras348 <rtaveras2@students.pccc.edu>
2024-02-16 19:35:30 -0500

    lab 3 finished

---
+++ labs/lab3/challenge_lab3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "The current date is: " 
+date -R 
+echo ' '
+echo "Some information about my system: " 
+uname -a       
+echo ' '
+echo "RAM Information " 
+free -m
+echo ' ' 
+echo "Challenge Question Complete"
